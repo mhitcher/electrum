bd59d62d0b97bc945eb01057dd0fb279d062e5b6
Janus <ysangkok@gmail.com>
2018-04-16 21:18:25 +0200

    lightning-hub: remove path hack, use relative imports

--- lib/ln/rpc_pb2.py
+++ lib/ln/rpc_pb2.py
@@ -14,7 +14,7 @@ from google.protobuf import descriptor_pb2
 _sym_db = _symbol_database.Default()
 
 
-from google.api import annotations_pb2 as google_dot_api_dot_annotations__pb2
+from .google.api import annotations_pb2 as google_dot_api_dot_annotations__pb2
 
 
 DESCRIPTOR = _descriptor.FileDescriptor(
